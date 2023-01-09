bc6c01fd03dec2969d5fb1a2da4a9e807c02f61f
Nathan Turkson <nathanturkson@gmail.com>
2023-01-09 23:43:36 +0000

    Feedback State Bug Fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let ratingsButtons = document.querySelectorAll(".ratings span");
-let ratingState = document.getElementById("ratingState");
-let feedbackState = document.getElementById("feedbackState");
+let ratingState = document.querySelector(".ratingState");
+let feedbackState = document.querySelector(".feedbackState");
+let submitButton = document.querySelector(".submit-Button");
 
 
 
@@ -9,15 +10,23 @@ for (let i = 0; i < ratingsButtons.length; i++) {
     let buttonInnerHTML = ratingsButtons[i].innerHTML;
     clickedButton.addEventListener("click", function () {
         clickedButton.classList.toggle("clicked");
-        ratingState.classList.add("nodisplay");
-        feedbackState.classList.add("display");
-
 
     })
-  
-
-    
 }
+
+
+
+
+submitButton.addEventListener("click", function () {
+
+    ratingState.classList.add("nodisplay");
+    feedbackState.classList.add("display");
+
+})
+
+
+
+
 
 
 
