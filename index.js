8233444ad16fe013c3ecb7f717f404886da0ff58
Nathan Turkson <nathanturkson@gmail.com>
2023-01-09 00:04:53 +0000

    Clicked Button States

---
+++ index.js
@@ -0,0 +1,13 @@
+let ratingsButtons = document.querySelectorAll(".ratings span");
+
+for (let i = 0; i < ratingsButtons.length; i++) {
+    let clickedButton = ratingsButtons[i];
+    clickedButton.addEventListener("click", function () {
+        clickedButton.classList.toggle("clicked");
+    })
+  
+
+    
+}
+
+
