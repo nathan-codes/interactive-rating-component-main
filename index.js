c25b215adde52c54f7e5668dc8c622b0b402268b
Nathan Turkson <nathanturkson@gmail.com>
2023-01-09 23:30:33 +0000

    Feedback State Working

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 let ratingsButtons = document.querySelectorAll(".ratings span");
+let ratingState = document.getElementById("ratingState");
+let feedbackState = document.getElementById("feedbackState");
+
+
 
 for (let i = 0; i < ratingsButtons.length; i++) {
     let clickedButton = ratingsButtons[i];
+    let buttonInnerHTML = ratingsButtons[i].innerHTML;
     clickedButton.addEventListener("click", function () {
         clickedButton.classList.toggle("clicked");
-        
+        ratingState.classList.add("nodisplay");
+        feedbackState.classList.add("display");
+
+
     })
   
 
     
 }
+
+
+
 
 
