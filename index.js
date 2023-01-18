5c4e8255521878b29e23aebf79af771d8840467c
Nathan Turkson <nathanturkson@gmail.com>
2023-01-18 16:49:35 +0000

    Refactored Code

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-let ratingsButtons = document.querySelectorAll(".ratings span");
-let ratingState = document.querySelector(".ratingState");
-let feedbackState = document.querySelector(".feedbackState");
-let submitButton = document.querySelector(".submit-Button");
+const ratingsButtons = document.querySelectorAll(".ratings span");
+const ratingState = document.querySelector(".ratingState");
+const feedbackState = document.querySelector(".feedbackState");
+const submitButton = document.querySelector(".submit-Button");
 
 
 
 
 for (let i = 0; i < ratingsButtons.length; i++) {
+    
+
     let clickedButton = ratingsButtons[i];
     let buttonInnerHTML = ratingsButtons[i].innerHTML;
     clickedButton.addEventListener("click", function () {
-        clickedButton.classList.toggle("clicked");
-        document.querySelector(".ratingNumber").innerHTML = buttonInnerHTML; 
+    document.querySelector(".ratingNumber").innerHTML = buttonInnerHTML; 
 
     })
 }
@@ -23,6 +24,11 @@ submitButton.addEventListener("click", function () {
 
     ratingState.classList.add("nodisplay");
     feedbackState.classList.add("display");
+
+    clickedButton.addEventListener("click", function () {
+        document.querySelector(".ratingNumber").innerHTML = buttonInnerHTML;
+
+    })
 
 })
 
